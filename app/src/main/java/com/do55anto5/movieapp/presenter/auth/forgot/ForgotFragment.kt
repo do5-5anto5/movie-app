e90797311275a694d098e06b644d6ec5fe1880fd
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-05-27 20:40:20 -0300

    implement forgot fragment layout

---
+++ app/src/main/java/com/do55anto5/movieapp/presenter/auth/forgot/ForgotFragment.kt
@@ -0,0 +1,31 @@
+package com.do55anto5.movieapp.presenter.auth.forgot
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.do55anto5.movieapp.R
+import com.do55anto5.movieapp.databinding.FragmentForgotBinding
+
+class ForgotFragment : Fragment() {
+    private var _binding: FragmentForgotBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentForgotBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
