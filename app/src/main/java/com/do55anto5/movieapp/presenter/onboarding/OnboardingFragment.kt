79ff4d30729d0fb6fb1bd8bdaf6f5502af820c9a
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-05-26 22:27:37 -0300

    add onboarding fragment  - initial implementation

---
+++ app/src/main/java/com/do55anto5/movieapp/presenter/onboarding/OnboardingFragment.kt
@@ -0,0 +1,32 @@
+package com.do55anto5.movieapp.presenter.onboarding
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.do55anto5.movieapp.databinding.FragmentOnboardingBinding
+
+class OnboardingFragment : Fragment() {
+
+    private var _binding: FragmentOnboardingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOnboardingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
