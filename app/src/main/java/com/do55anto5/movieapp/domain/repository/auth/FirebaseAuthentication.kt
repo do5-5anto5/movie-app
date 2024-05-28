6cc632c7da2b95fff82e21ca8c6531bc4e4c5d1e
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-05-27 23:09:55 -0300

    initial implement Firebase authentication service

---
+++ app/src/main/java/com/do55anto5/movieapp/domain/repository/auth/FirebaseAuthentication.kt
@@ -0,0 +1,10 @@
+package com.do55anto5.movieapp.domain.repository.auth
+
+interface FirebaseAuthentication {
+
+    suspend fun login(email: String, password: String)
+
+    suspend fun register(email: String, password: String)
+
+    suspend fun forgot(email: String)
+}
