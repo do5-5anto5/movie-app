c7c235951342ecc52c03965447043be5732853c7
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-05-28 18:41:52 -0300

    create authentication graph

---
+++ app/src/main/java/com/do55anto5/movieapp/presenter/auth/activity/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.do55anto5.movieapp.presenter.auth.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.do55anto5.movieapp.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
