3cbdc1f027431fb21fd83c485230c32a34921263
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-06-01 15:10:00 -0300

    add ServiceApi interface

---
+++ app/src/main/java/com/do55anto5/movieapp/data/api/ServiceApi.kt
@@ -0,0 +1,23 @@
+package com.do55anto5.movieapp.data.api
+
+import com.do55anto5.movieapp.data.model.GenresResponse
+import com.do55anto5.movieapp.data.model.MovieResponse
+import com.do55anto5.movieapp.data.model.RemoteBasePagination
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ServiceApi {
+
+    @GET("genre/movie/list")
+    suspend fun getGenres(
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String
+    ) : GenresResponse
+
+    @GET("discover/movie")
+    suspend fun getMoviesByGenre(
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String?,
+        @Query("with_genres") genreId: Int?
+    ) : RemoteBasePagination<List<MovieResponse>>
+}
