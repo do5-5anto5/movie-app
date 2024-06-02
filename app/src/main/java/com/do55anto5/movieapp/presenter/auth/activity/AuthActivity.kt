3aa6b1884319ad71a2728b00838584d0438ca2ab
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-06-02 13:21:41 -0300

    dinamizing status bar visibility

--- app/src/main/java/com/do55anto5/movieapp/presenter/auth/activity/AuthActivity.kt
+++ app/src/main/java/com/do55anto5/movieapp/presenter/auth/activity/AuthActivity.kt
@@ -1,14 +1,48 @@
 package com.do55anto5.movieapp.presenter.auth.activity
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.do55anto5.movieapp.R
+import com.do55anto5.movieapp.databinding.ActivityAuthBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AuthActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
+
+    private lateinit var binding: ActivityAuthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setStatusBarTranslucent()
+
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id != R.id.onboardingFragment) {
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            }
+        }
+    }
+
+    private fun setStatusBarTranslucent() {
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
     }
 }
