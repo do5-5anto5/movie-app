77d129c29762448e2ffd4d3da65e34bd9165407b
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-06-03 17:13:12 -0300

    add MovieGenreViewModel, listing movies on MovieGenreFragment

--- app/src/main/java/com/do55anto5/movieapp/presenter/main/movie_genre/MovieGenreFragment.kt
+++ app/src/main/java/com/do55anto5/movieapp/presenter/main/movie_genre/MovieGenreFragment.kt
@@ -1,13 +1,17 @@
 package com.do55anto5.movieapp.presenter.main.movie_genre
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.GridLayoutManager
 import com.do55anto5.movieapp.databinding.FragmentMovieGenreBinding
+import com.do55anto5.movieapp.presenter.main.bottom_bar.home.adapter.MovieAdapter
+import com.do55anto5.movieapp.util.StateView
 import com.do55anto5.movieapp.util.initToolbar
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,6 +22,10 @@ class MovieGenreFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val args: MovieGenreFragmentArgs by navArgs()
+
+    private lateinit var movieAdapter: MovieAdapter
+
+    private val viewModel: MovieGenreViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,9 +38,40 @@ class MovieGenreFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Log.i("INFOTEST", "ID = ${args.genreId}")
-
         initToolbar(binding.toolbar)
+
+        initRecyclerView()
+
+        getMoviesByGenre()
+    }
+
+    private fun getMoviesByGenre() {
+
+        viewModel.getMoviesByGenre(args.genreId).observe(viewLifecycleOwner) { stateView ->
+            when (stateView) {
+                is StateView.Loading -> {
+                    binding.progressBar.isVisible = true
+                }
+
+                is StateView.Success -> {
+                    binding.progressBar.isVisible = false
+                   movieAdapter.submitList(stateView.data)
+                }
+
+                is StateView.Error -> {
+                    binding.progressBar.isVisible = false
+                }
+            }
+        }
+    }
+
+    private fun initRecyclerView() {
+        movieAdapter = MovieAdapter(requireContext())
+        with(binding.recyclerMovies) {
+            layoutManager = GridLayoutManager(requireContext(), 2)
+            setHasFixedSize(true)
+            adapter = movieAdapter
+        }
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/do55anto5/movieapp/presenter/main/movie_genre/MovieGenreViewModel.kt
@@ -0,0 +1,42 @@
+package com.do55anto5.movieapp.presenter.main.movie_genre
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.do55anto5.movieapp.BuildConfig
+import com.do55anto5.movieapp.domain.usecase.movie.GetMoviesByGenreUseCase
+import com.do55anto5.movieapp.util.Constants
+import com.do55anto5.movieapp.util.StateView
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import retrofit2.HttpException
+import javax.inject.Inject
+
+@HiltViewModel
+class MovieGenreViewModel @Inject constructor(
+    private val getMoviesByGenreUseCase: GetMoviesByGenreUseCase
+) : ViewModel(){
+
+    fun getMoviesByGenre(genreId: Int?) = liveData(Dispatchers.IO) {
+        try {
+
+            emit(StateView.Loading())
+
+            val movies = getMoviesByGenreUseCase.invoke(
+                apiKey = BuildConfig.API_KEY,
+                language = Constants.Movie.LANGUAGE,
+                genreId = genreId
+            )
+
+
+            emit(StateView.Success(movies))
+
+        } catch (e: HttpException) {
+            e.printStackTrace()
+            emit(StateView.Error(e.message))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emit(StateView.Error(e.message))
+        }
+    }
+
+}
