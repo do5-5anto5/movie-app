21f3877d2f98a189d672e2a0b73d10a47d79208b
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-06-05 09:41:36 -0300

    add MovieDetailsFragment and MovieDetailsViewModel

---
+++ app/src/main/java/com/do55anto5/movieapp/presenter/main/movie_details/MovieDetailsFragment.kt
@@ -0,0 +1,28 @@
+package com.do55anto5.movieapp.presenter.main.movie_details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.do55anto5.movieapp.databinding.FragmentMovieDetailsBinding
+
+class MovieDetailsFragment : Fragment() {
+
+    private var _binding: FragmentMovieDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMovieDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/do55anto5/movieapp/presenter/main/movie_details/MovieDetailsViewModel.kt
@@ -0,0 +1,10 @@
+package com.do55anto5.movieapp.presenter.main.movie_details
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MovieDetailsViewModel @Inject constructor(
+) : ViewModel() {
+}
