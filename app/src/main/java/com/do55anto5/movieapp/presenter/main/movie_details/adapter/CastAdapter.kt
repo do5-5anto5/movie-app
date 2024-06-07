3a3594aafa5d9c735faad821c5dbb9482ec14da7
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-06-07 17:35:57 -0300

    add CastAdapter

---
+++ app/src/main/java/com/do55anto5/movieapp/presenter/main/movie_details/adapter/CastAdapter.kt
@@ -0,0 +1,57 @@
+package com.do55anto5.movieapp.presenter.main.movie_details.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.do55anto5.movieapp.R
+import com.do55anto5.movieapp.databinding.CastItemBinding
+import com.do55anto5.movieapp.domain.model.Person
+
+class CastAdapter : ListAdapter<Person, CastAdapter.MyViewHolder>(DIFF_CALLBACK) {
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Person>() {
+            override fun areItemsTheSame(
+                oldItem: Person,
+                newItem: Person
+            ): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(
+                oldItem: Person,
+                newItem: Person
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(
+            CastItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val person = getItem(position)
+
+        Glide.with(holder.binding.root.context)
+            .load("https://image.tmdb.org/t/p/w500${person.profilePath}")
+            .error(R.drawable.bg_shadow)
+            .into(holder.binding.imagePerson)
+
+        holder.binding.personName.text = person.name
+    }
+
+    inner class MyViewHolder(val binding: CastItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+}
