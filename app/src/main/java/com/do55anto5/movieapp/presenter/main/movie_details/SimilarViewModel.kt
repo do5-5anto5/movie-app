af284ec2c50345b256dc795006a4725e0c7ba7d0
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-06-10 18:25:14 -0300

    add SimilarViewModel

---
+++ app/src/main/java/com/do55anto5/movieapp/presenter/main/movie_details/SimilarViewModel.kt
@@ -0,0 +1,42 @@
+package com.do55anto5.movieapp.presenter.main.movie_details
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.do55anto5.movieapp.BuildConfig
+import com.do55anto5.movieapp.domain.usecase.movie.GetSimilarUseCase
+import com.do55anto5.movieapp.util.Constants
+import com.do55anto5.movieapp.util.StateView
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import retrofit2.HttpException
+import javax.inject.Inject
+
+@HiltViewModel
+class SimilarViewModel @Inject constructor(
+    private val getSimilarUseCase: GetSimilarUseCase
+): ViewModel() {
+
+    fun getSimilar(movieId: Int?) = liveData(Dispatchers.IO) {
+        try {
+
+            emit(StateView.Loading())
+
+            val genres = getSimilarUseCase.invoke(
+                apiKey = BuildConfig.API_KEY,
+                language = Constants.Movie.LANGUAGE,
+                movieId = movieId
+            )
+
+
+            emit(StateView.Success(genres))
+
+        } catch (e: HttpException) {
+            e.printStackTrace()
+            emit(StateView.Error(e.message))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emit(StateView.Error(e.message))
+        }
+    }
+
+}
