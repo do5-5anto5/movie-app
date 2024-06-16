0e0aedbcb81dea6ab9f52005e6ee4c9affa97eeb
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-06-16 11:30:16 -0300

    set toolbar on downloads screen

--- app/src/main/java/com/do55anto5/movieapp/presenter/main/bottom_bar/download/DownloadFragment.kt
+++ app/src/main/java/com/do55anto5/movieapp/presenter/main/bottom_bar/download/DownloadFragment.kt
@@ -2,15 +2,26 @@ package com.do55anto5.movieapp.presenter.main.bottom_bar.download
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import com.do55anto5.movieapp.MainGraphDirections
+import com.do55anto5.movieapp.R
 import com.do55anto5.movieapp.databinding.FragmentDownloadBinding
+import com.do55anto5.movieapp.presenter.main.bottom_bar.download.adapter.DownloadMovieAdapter
+import com.do55anto5.movieapp.util.hideKeyboard
+import com.ferfalk.simplesearchview.SimpleSearchView
 
 class DownloadFragment : Fragment() {
 
     private var _binding: FragmentDownloadBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var mAdapter: DownloadMovieAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +30,84 @@ class DownloadFragment : Fragment() {
     ): View {
         _binding = FragmentDownloadBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRecyclerView()
+
+        initSearchView()
+    }
+
+    private fun initSearchView() {
+        binding.simpleSearchView.setOnQueryTextListener(object : SimpleSearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+
+                hideKeyboard()
+
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextCleared(): Boolean {
+                return false
+            }
+        })
+
+        binding.simpleSearchView.setOnSearchViewListener(object : SimpleSearchView.SearchViewListener {
+            override fun onSearchViewShown() {
+            }
+
+            override fun onSearchViewClosed() {
+//                getMoviesByGenre()
+            }
+
+            override fun onSearchViewShownAnimation() {
+            }
+
+            override fun onSearchViewClosedAnimation() {
+            }
+        })
+    }
+
+    private fun initRecyclerView() {
+        mAdapter = DownloadMovieAdapter(
+            context = requireContext(),
+            detailsClickListener = { movieId ->
+                movieId?.let {
+                    val action = MainGraphDirections.actionGlobalMovieDetailsFragment(movieId)
+                    findNavController().navigate(action)
+                }
+            },
+            deleteClickListener = { movieId ->
+                movieId?.let {
+                    val action = MainGraphDirections.actionGlobalMovieDetailsFragment(movieId)
+                    findNavController().navigate(action)
+                }
+            }
+        )
+        with(binding.rvMovies) {
+            layoutManager = GridLayoutManager(requireContext(), 2)
+            setHasFixedSize(true)
+            adapter = mAdapter
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_search_view, menu)
+        val item = menu.findItem(R.id.action_search)
+        binding.simpleSearchView.setMenuItem(item)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
     override fun onDestroyView() {
