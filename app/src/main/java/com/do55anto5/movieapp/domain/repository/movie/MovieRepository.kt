1b06aca22683eafbbaf5242525d0f11af097d66d
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-06-28 19:24:32 -0300

    add function getMoviesByGenrePagination

--- app/src/main/java/com/do55anto5/movieapp/domain/repository/movie/MovieRepository.kt
+++ app/src/main/java/com/do55anto5/movieapp/domain/repository/movie/MovieRepository.kt
@@ -3,14 +3,15 @@ package com.do55anto5.movieapp.domain.repository.movie
 import androidx.paging.PagingSource
 import com.do55anto5.movieapp.data.model.GenresResponse
 import com.do55anto5.movieapp.data.model.MovieResponse
+import com.do55anto5.movieapp.data.model.RemoteBasePagination
 
 interface MovieRepository {
 
     suspend fun getGenres(): GenresResponse
 
-    fun getMoviesByGenre(
-        genreId: Int?
-    ): PagingSource<Int, MovieResponse>
+    fun getMoviesByGenrePagination(genreId: Int?): PagingSource<Int, MovieResponse>
+
+    suspend fun getMoviesByGenre(genreId: Int?): RemoteBasePagination<List<MovieResponse>>
 
     fun searchMovies(query: String?): PagingSource<Int, MovieResponse>
 
