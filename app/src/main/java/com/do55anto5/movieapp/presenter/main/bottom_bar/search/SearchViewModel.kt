3a545b3b71b411ad8112d38bb3ecda45a9879f4b
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-06-29 19:34:44 -0300

    adjust constraint top of layout_empty, optimize imports on SearchViewModel

--- app/src/main/java/com/do55anto5/movieapp/presenter/main/bottom_bar/search/SearchViewModel.kt
+++ app/src/main/java/com/do55anto5/movieapp/presenter/main/bottom_bar/search/SearchViewModel.kt
@@ -1,18 +1,13 @@
 package com.do55anto5.movieapp.presenter.main.bottom_bar.search
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.do55anto5.movieapp.domain.model.Movie
 import com.do55anto5.movieapp.domain.usecase.movie.SearchMoviesUseCase
-import com.do55anto5.movieapp.util.StateView
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
-import retrofit2.HttpException
 import javax.inject.Inject
 
 @HiltViewModel
