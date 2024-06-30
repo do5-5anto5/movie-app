719e8e1ef3f30c5fa915ca5e41629d00adf8272f
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-06-30 20:40:56 -0300

    add MenuProfile ProfileMenuAdapter

---
+++ app/src/main/java/com/do55anto5/movieapp/presenter/main/bottom_bar/profile/adapter/ProfileMenuAdapter.kt
@@ -0,0 +1,48 @@
+package com.do55anto5.movieapp.presenter.main.bottom_bar.profile.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.do55anto5.movieapp.databinding.ItemUserProfileBinding
+import com.do55anto5.movieapp.domain.model.MenuProfile
+import com.do55anto5.movieapp.domain.model.MenuProfileType
+
+class ProfileMenuAdapter(
+    private val items: List<MenuProfile>,
+    private val context: Context,
+    private val onClick: (MenuProfileType) -> Unit
+): RecyclerView.Adapter<ProfileMenuAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(
+            ItemUserProfileBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val item = items[position]
+
+       with(holder) {
+           binding.txtItemProfile.text = context.getString(item.text)
+           binding.imgItemProfile.setImageDrawable(
+               ContextCompat.getDrawable(context, item.icon)
+           )
+           itemView.setOnClickListener {
+               onClick(item.type)
+           }
+       }
+    }
+
+    inner class MyViewHolder(val binding: ItemUserProfileBinding): RecyclerView.ViewHolder(binding.root)
+
+}
