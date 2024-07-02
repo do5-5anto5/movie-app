b1ea7cfd6a8e0aaab8109483e2804f8a7c5e93d5
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-07-02 20:28:46 -0300

    implement validateData()

--- app/src/main/java/com/do55anto5/movieapp/presenter/main/bottom_bar/profile/edit/EditProfileFragment.kt
+++ app/src/main/java/com/do55anto5/movieapp/presenter/main/bottom_bar/profile/edit/EditProfileFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.do55anto5.movieapp.R
 import com.do55anto5.movieapp.databinding.FragmentEditProfileBinding
 import com.do55anto5.movieapp.util.initToolbar
+import com.do55anto5.movieapp.util.showSnackBar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -29,6 +31,48 @@ class EditProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initToolbar(binding.toolbar)
+
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnUpdate.setOnClickListener {
+            validateData()
+        }
+    }
+
+    private fun validateData() {
+
+        val name = binding.editNameFirstName.text.toString()
+        val surname = binding.editSurname.text.toString()
+        val phoneNumber = binding.editPhoneNumber.text.toString()
+        val gender = binding.editGender.text.toString()
+        val country = binding.editCountry.text.toString()
+
+        if (name.isEmpty()) {
+            showSnackBar(R.string.text_name_empty)
+            return
+        }
+
+        if (surname.isEmpty()) {
+            showSnackBar(R.string.text_name_empty)
+            return
+        }
+
+        if (phoneNumber.isEmpty()) {
+            showSnackBar(R.string.text_phone_invalid)
+            return
+        }
+
+        if (gender.isEmpty()) {
+            showSnackBar(R.string.text_gender_empty)
+            return
+        }
+
+        if (country.isEmpty()) {
+            showSnackBar(R.string.text_country_empty)
+            return
+        }
     }
 
     override fun onDestroyView() {
