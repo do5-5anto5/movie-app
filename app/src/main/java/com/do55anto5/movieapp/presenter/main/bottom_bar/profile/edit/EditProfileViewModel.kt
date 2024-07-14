83866debedf15150c072d88ca8d78bfb043a788f
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-07-14 14:57:09 -0300

    implement getUserUseCase to EditProfileViewModel. Also remove invoke() to turn code more readable

--- app/src/main/java/com/do55anto5/movieapp/presenter/main/bottom_bar/profile/edit/EditProfileViewModel.kt
+++ app/src/main/java/com/do55anto5/movieapp/presenter/main/bottom_bar/profile/edit/EditProfileViewModel.kt
@@ -3,6 +3,7 @@ package com.do55anto5.movieapp.presenter.main.bottom_bar.profile.edit
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.do55anto5.movieapp.domain.model.user.User
+import com.do55anto5.movieapp.domain.usecase.user.GetUserUseCase
 import com.do55anto5.movieapp.domain.usecase.user.UserUpdateUseCase
 import com.do55anto5.movieapp.util.StateView
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -11,18 +12,33 @@ import javax.inject.Inject
 
 @HiltViewModel
 class EditProfileViewModel @Inject constructor(
-    private val userUpdateUseCase: UserUpdateUseCase
+    private val userUpdateUseCase: UserUpdateUseCase,
+    private val getUserUseCase: GetUserUseCase,
 ) : ViewModel() {
 
     fun update(user: User) = liveData(Dispatchers.IO) {
         try {
             emit(StateView.Loading())
 
-            userUpdateUseCase.invoke(user)
+            userUpdateUseCase(user)
 
             kotlinx.coroutines.delay(3000)
 
             emit(StateView.Success(Unit))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emit(StateView.Error(e.message))
+        }
+    }
+    fun get() = liveData(Dispatchers.IO) {
+        try {
+            emit(StateView.Loading())
+
+            val user = getUserUseCase()
+
+            kotlinx.coroutines.delay(3000)
+
+            emit(StateView.Success(user))
         } catch (e: Exception) {
             e.printStackTrace()
             emit(StateView.Error(e.message))
