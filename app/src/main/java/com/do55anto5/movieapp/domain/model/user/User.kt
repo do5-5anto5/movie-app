e6d56853e1714cdd4d454227705a9674d516793d
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-07-14 12:12:43 -0300

    Add User class

---
+++ app/src/main/java/com/do55anto5/movieapp/domain/model/user/User.kt
@@ -0,0 +1,12 @@
+package com.do55anto5.movieapp.domain.model.user
+
+data class User(
+    val id: String? = null,
+    val photoUrl: String? = null,
+    val firstName: String? = null,
+    val surname: String? = null,
+    val email: String? = null,
+    val phoneNumber: String? = null,
+    val gender: String? = null,
+    val country: String? = null
+)
