0857eae9be742534b2fd3ebd12f9ae96ad4fe64d
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-07-14 12:13:19 -0300

    Add UserUpdateUseCase

---
+++ app/src/main/java/com/do55anto5/movieapp/domain/usecase/user/UserUpdateUseCase.kt
@@ -0,0 +1,15 @@
+package com.do55anto5.movieapp.domain.usecase.user
+
+import com.do55anto5.movieapp.domain.model.user.User
+import com.do55anto5.movieapp.domain.repository.user.UserRepository
+import javax.inject.Inject
+
+class UserUpdateUseCase @Inject constructor(
+    private val userRepository: UserRepository
+) {
+
+    suspend fun invoke(user: User) {
+        userRepository.update(user)
+    }
+
+}
