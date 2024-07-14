23866dcc9a04960465ca33761d3ee5176e6d1958
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-07-14 14:53:26 -0300

    Use suspendCancellableCoroutine to return User object from user repository

--- app/src/main/java/com/do55anto5/movieapp/data/repository/user/UserRepositoryImpl.kt
+++ app/src/main/java/com/do55anto5/movieapp/data/repository/user/UserRepositoryImpl.kt
@@ -3,7 +3,12 @@ package com.do55anto5.movieapp.data.repository.user
 import com.do55anto5.movieapp.domain.model.user.User
 import com.do55anto5.movieapp.domain.repository.user.UserRepository
 import com.do55anto5.movieapp.util.FirebaseHelper
+import com.do55anto5.movieapp.util.safeResume
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.coroutines.suspendCancellableCoroutine
 import javax.inject.Inject
 import kotlin.coroutines.suspendCoroutine
 
@@ -26,6 +31,28 @@ class UserRepositoryImpl @Inject constructor(
                         task.exception?.let { continuation.resumeWith(Result.failure(it)) }
                     }
                 }
+        }
+    }
+
+    override suspend fun get(): User {
+        return suspendCancellableCoroutine { continuation ->
+            profileRef
+                .child(FirebaseHelper.getUserId())
+                .addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        val user = snapshot.getValue(User::class.java)
+                        user?.let {
+                        continuation.safeResume(it)
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        error.toException().let {
+                            continuation.resumeWith(Result.failure(it))
+                        }
+                    }
+
+                })
         }
     }
 
