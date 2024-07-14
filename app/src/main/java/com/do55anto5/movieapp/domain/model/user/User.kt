0760fb41faeae162bc9f9817e03c406701145968
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-07-14 15:47:55 -0300

    Add @IgnoreExtraProperties to User class

--- app/src/main/java/com/do55anto5/movieapp/domain/model/user/User.kt
+++ app/src/main/java/com/do55anto5/movieapp/domain/model/user/User.kt
@@ -1,9 +1,11 @@
 package com.do55anto5.movieapp.domain.model.user
 
 import android.os.Parcelable
+import com.google.firebase.database.IgnoreExtraProperties
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@IgnoreExtraProperties
 data class User(
     val id: String? = "",
     val photoUrl: String? = "",
